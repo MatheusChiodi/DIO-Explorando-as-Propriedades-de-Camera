f76d253451b86026d49cbbf49f6b9b3404b968bd
Matheus Chiodi <matheuschiodi20@gmail.com>
2024-01-09 13:30:39 -0300

    Reorganizando o Código

---
+++ src/components/CameraView.tsx
@@ -0,0 +1,43 @@
+import { Camera, FlashMode, AutoFocus, CameraType } from 'expo-camera';
+import { View, TouchableOpacity, StyleSheet, Text } from 'react-native';
+
+export default function CameraView({ type, onFlipCamera }) {
+  return (
+    <Camera
+      style={{ flex: 1 }}
+      type={type}
+      ratio={'16:9'}
+      zoom={0}
+      flashMode={FlashMode.off}
+      autoFocus={AutoFocus.on}
+    >
+      <View style={styles.mainView}>
+        <TouchableOpacity style={styles.flipArea} onPress={() => onFlipCamera}>
+          <Text style={styles.flipText}>Flip Camera</Text>
+        </TouchableOpacity>
+      </View>
+    </Camera>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  mainView: {
+    flex: 1,
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+  },
+  flipArea: {
+    position: 'absolute',
+    bottom: 20,
+    left: 20,
+  },
+  flipText: {
+    fontSize: 20,
+    marginBottom: 13,
+    color: 'white',
+  },
+});
