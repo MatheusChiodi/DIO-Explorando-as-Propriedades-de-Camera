2e302ec769b753df8fe805d4f6ed5dc306558656
Matheus Chiodi <matheuschiodi20@gmail.com>
2024-01-09 16:47:06 -0300

    ajustando o Media Library

--- App.tsx
+++ App.tsx
@@ -1,21 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Camera, CameraType } from 'expo-camera';
+import * as MediaLibrary from 'expo-media-library';
+
 import CameraView from './src/components/CameraView';
 
 export default function App() {
   const [type, setType] = useState<CameraType>(CameraType.back);
   const [permission, requestPermission] = Camera.useCameraPermissions();
-  const [hasPermission, setHasPermission] = useState<boolean>(false);
+  const [hasCameraPermission, setHasCameraPermission] =
+    useState<boolean>(false);
+  const [hasMediaPermission, setHasMediaPermission] = useState<boolean>(false);
 
   useEffect(() => {
     (async () => {
       const { status } = await Camera.requestCameraPermissionsAsync();
-      setHasPermission(status === 'granted');
+      setHasCameraPermission(status === 'granted');
+    })();
+    (async () => {
+      const { status } = await MediaLibrary.requestPermissionsAsync();
+      setHasMediaPermission(status === 'granted');
     })();
   }, []);
 
-  if (hasPermission === false) {
+  if (hasCameraPermission === false) {
+    return <View />;
+  }
+
+  if (hasMediaPermission === false) {
     return <View />;
   }
 
--- src/components/CameraView/index.tsx
+++ src/components/CameraView/index.tsx
@@ -1,18 +1,30 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { View, TouchableOpacity, StyleSheet, Text } from 'react-native';
+import React, { useRef, useState } from 'react';
+import { View, TouchableOpacity, Text, Modal, Image } from 'react-native';
 import { Camera, FlashMode, AutoFocus } from 'expo-camera';
 import CameraViewProps from './interfaces/CameraViewProps';
+import * as MediaLibrary from 'expo-media-library';
 
 import styles from './styles';
 
 export default function CameraView({ type, onFlipCamera }: CameraViewProps) {
   const camRef = useRef<Camera>(null);
   const [capturedPhoto, setCapturedPhoto] = useState<string | null>(null);
+  const [modalIsOpen, setModalIsOpen] = useState<boolean>(false);
 
   async function takePicture() {
     if (camRef && camRef.current) {
       const data = await camRef.current.takePictureAsync();
+      setModalIsOpen(true);
       setCapturedPhoto(data.uri);
+    }
+  }
+
+  async function savePicture() {
+    if (capturedPhoto != null) {
+      await MediaLibrary.saveToLibraryAsync(capturedPhoto).then(() => {
+        setModalIsOpen(false);
+        setCapturedPhoto(null);
+      });
     }
   }
 
@@ -35,6 +47,42 @@ export default function CameraView({ type, onFlipCamera }: CameraViewProps) {
           <Text style={styles.takePhotoText}>Take Picture</Text>
         </TouchableOpacity>
       </View>
+
+      {capturedPhoto && (
+        <Modal animationType="slide" transparent={false} visible={modalIsOpen}>
+          <View
+            style={{
+              flex: 1,
+              justifyContent: 'center',
+              alignItems: 'center',
+              margin: 20,
+            }}
+          >
+            <View
+              style={{
+                display: 'flex',
+                flexDirection: 'row',
+              }}
+            >
+              <TouchableOpacity
+                style={{ margin: 10 }}
+                onPress={() => setModalIsOpen(false)}
+              >
+                <Text style={{ fontSize: 20 }}>Close</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity style={{ margin: 10 }} onPress={savePicture}>
+                <Text style={{ fontSize: 20 }}>Save</Text>
+              </TouchableOpacity>
+            </View>
+
+            <Image
+              style={{ width: '100%', height: 300, borderRadius: 20 }}
+              source={{ uri: capturedPhoto }}
+            />
+          </View>
+        </Modal>
+      )}
     </Camera>
   );
 }
